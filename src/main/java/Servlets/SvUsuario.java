9d554fa0a5011cd7c97904eb7d921b2ed6566c6f
jeanpcot <jean.cotera@gmail.com>
2024-04-27 23:53:15 -0500

    Actualización del servlet Usuario

--- src/main/java/Servlets/SvUsuario.java
+++ src/main/java/Servlets/SvUsuario.java
@@ -19,33 +19,26 @@ public class SvUsuario extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //Sesion que se crea automáticamente al entrar a la web por el usuario
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        List<Usuario> listaCompradores = entityManager.createQuery("SELECT c.id, c.nombre, c.apellido FROM Usuario c", Usuario.class).getResultList();
-        HttpSession sesion = request.getSession();
-        sesion.setAttribute("listaCompradores", listaCompradores);
-        response.sendRedirect("busqueda.jsp");
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String id = request.getParameter("id_usuario");
-        String nombre = request.getParameter("nombre");
-        String apellido = request.getParameter("apellido");
-
-        Usuario comprador = new Usuario();
-        comprador.setNombre(nombre);
-        comprador.setApellido(apellido);
-
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
-        entityManager.persist(comprador);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        entityManagerFactory.close();
-        response.sendRedirect("index.jsp");
+        List<Usuario> listaUsuarios = entityManager.createQuery("SELECT c.id, c.nombre, c.apellido FROM Usuario c", Usuario.class).getResultList();
+        HttpSession sesion = request.getSession();
+
+
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+
+
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.validar(nombre, apellido))
+                response.sendRedirect("busqueda.jsp");
+        }
+
+        response.sendError(404);
     }
 }
